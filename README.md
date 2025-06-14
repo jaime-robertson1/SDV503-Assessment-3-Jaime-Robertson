633778a5f9183921fe75489fa413a0e1d49cfe8c
jaime-robertson1 <jaime-robertson@live.nmit.ac.nz>
2025-06-14 18:19:28 +1200

    Completing comments in my code and adding to the readME file, including a user manual

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # SDV503-Assessment-3-Jaime-Robertson
 
-// Coding checklist 
-// - Comments to ensure code is easily understood. But don't add comments for things that are obvious 
-// - Consistent indentation 
-// - Follow DRY principles
-// - Use camelCase consistently
-// - Try to make lines of code or comments not too long and hard to read
+Coding checklist 
+ - Comments to ensure code is easily understood. But don't add comments for things that are obvious 
+ - Consistent indentation 
+ - Follow DRY principles
+ - Use camelCase consistently
+ - Try to make lines of code or comments not too long and hard to read
+
+**User Manual: **
+
--- patientprofile.js
+++ patientprofile.js
@@ -35,15 +35,15 @@ function showMenu() {
   console.log('1. Patient Login');                                    // Option 1: Patient Login
   console.log('2. Healthcare Provider Login');                        // Option 2: Healthcare Provider Login                
   console.log('3. Exit');                                             // Option 3: Exit the app
-  rl.question('\nChoose an option (1, 2 or 3): ', handleMenu);        // Prompt user for menu choice
-}
+  rl.question('\nChoose an option (1, 2 or 3): ', handleMenu);        // Prompt user for menu choice, handleMenu() function
+}                                                                     // to be created below
 
 // Function to save patients array to the patients.json file
 function savePatients() {
   fs.writeFileSync(FILE, JSON.stringify(patients, null, 2))
 }
 
-// List of healthcare providers log in details.
+// Const of healthcare providers log in details.
 // This is not stored in a .json file because the details aren't going to be changing.
 const providers = [
   { username: 'drmatthews', password: 'matthews123'},
@@ -80,21 +80,21 @@ function loginUser (type) {
   rl.question('Username: ', username => {    // input from the user to enter username
     rl.question('Password: ', password => {  // input from the user to enter password 
       if (type === 'patient') {              // if the number selected is 1 for patient, 
-        const user = patients.find(p => p.username === username && p.password === password) // patients.find returns the first element in the array that satisfies the testing function.
-        if (user) {                             // in this case, the patient array is checked until it finds a case where the username and password match the ones inputted by the user.
-          console.log(`\nWelcome, ${user.name} (patient)!`)         // lets the user know they have successfully entered the patient menu
-          patientMenu(user)                                         // the user is directed to the patientMenu()
-        } else {                                                    // if the username and password cannot be found in the patient array, the person is not able to log in
-          console.log('Invalid credentials. Please try again. \n')  // they are then prompted to enter their details again in case they made a mistake.
-          showMenu()                                                // the main menu is shown so they can try again
+        const user = patients.find(p => p.username === username && p.password === password) // Patients.find returns the first element in the array that satisfies the testing function.
+        if (user) {                          // In this case, the patient array is checked until it finds a case where the username and password match the ones inputted by the user.
+          console.log(`\nWelcome, ${user.name} (patient)!`)         // Lets the user know they have successfully entered the patient menu
+          patientMenu(user)                                         // The user is directed to the patientMenu()
+        } else {                                                    // If the username and password cannot be found in the patient array, the person is not able to log in
+          console.log('Invalid credentials. Please try again. \n')  // They are then prompted to enter their details again in case they made a mistake.
+          showMenu()                                                // The main menu is shown so they can try again
         }
        } else if (type === 'provider') {                            // if the user logging in is not a patient, check if provider 
-            const user = providers.find(p => p.username === username && p.password === password)
+            const user = providers.find(p => p.username === username && p.password === password) // Same as above but it is checking the providers array for a match
             if (user) {
               console.log(`\nWelcome, ${user.username} (provider)!`) // lets the user know they have successfully entered the provider menu
               providerMenu()
           } else {
-            console.log('Invalid credentials. Please try again. \n') // if credentionals are wrong they're given a chance to input them again
+            console.log('Invalid credentials. Please try again. \n') // If credentionals are wrong they're given a chance to input them again
             showMenu()
           }
         }
@@ -104,34 +104,34 @@ function loginUser (type) {
 
 // Function to show menu for patients after successfully logging in
 function patientMenu(patient) {
-  console.log('\n1. View My Profile') // three choices that the patient can make
-  console.log('2. Edit My Profile')
-  console.log('3. Logout')
-  rl.question('\nChoose an option: ', choice => {
+  console.log('\n1. View My Profile')  // Three choices that the patient can make, 1. View their profile
+  console.log('2. Edit My Profile')    // 2. Edits their profile
+  console.log('3. Logout')             // 3. Logs out the user 
+  rl.question('\nChoose an option: ', choice => { // User inputs choice 
     switch (choice.trim()) {
-      case '1':
+      case '1':                        // If 1 is selected, print patients profile details. Info is inserted from the relevant patient stored in the patients array.
         console.log(`\nName: ${patient.name}\nDob: ${patient.dob}\nContact: ${patient.contactnumber}\nPhoto: ${patient.photo}\nAllergies: ${patient.allergies}\nConditions: ${patient.conditions}\nMedications: ${patient.medications}\nSmoker: ${patient.smoker}`)
-        patientMenu(patient)
+        patientMenu(patient)           // After printing the info, show the patient menu options again
         break
       case '2':
-        editPatientProfile(patient, () => patientMenu(patient))
+        editPatientProfile(patient, () => patientMenu(patient))      // Show a new menu for editing info which will be created further down, editPatientProfile()
         break
       case '3':
-        showMenu()
+        showMenu()                     // Simulates logging out by going back to the main menu
         break
       default:
-        console.log('Invalid choice.')
-        patientMenu(patient)   
+        console.log('Invalid choice. Try again with a valid number.') // Displays a error message if the patient types in a number other than 1, 2 or 3
+        patientMenu(patient)                                          // Displays the menu again so they can try inputting a valid number 
     }
   })
 }
 
 // Function to show menu for providers after successfully logging in
 function providerMenu() {
-  console.log('\n1. View All Patients') // three choices that the provider can make
-  console.log('2. Edit a Patient Profile')
-  console.log('3. Logout')
-  rl.question('\nChoose an option: ', choice => {
+  console.log('\n1. View All Patients')    // Three choices that the provider can make, 1. View all their patients info
+  console.log('2. Edit a Patient Profile') // 2. Edits a patients profile
+  console.log('3. Logout')                 // 3. Logs out the provider 
+  rl.question('\nChoose an option: ', choice => { // User inputs choice 
     switch (choice.trim()) {
       case '1':
         patients.forEach((p, i) => {
@@ -154,7 +154,7 @@ function providerMenu() {
         showMenu()
         break
       default:
-        console.log('Invalid choice.')
+        console.log('Invalid choice. Try again with a valid number')
         providerMenu()
     }
   })
